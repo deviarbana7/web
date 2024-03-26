ed2ab9cdc6c0f7aa2f360832bfa6b9add62d230d
Devi Arbana <deviarbana7@gmail.com>
2024-03-26 10:19:11 +0100

    krjimi lidhjes me server local

---
+++ regjistrim.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$database = "prov";
+
+$con=mysqli_connect($servername, $username, $password, $database) OR die ('Provo perseri');
+
+?>
