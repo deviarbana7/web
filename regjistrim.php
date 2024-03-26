a69e654441355c47dd396620c0b7a3d263ad70e2
Devi Arbana <deviarbana7@gmail.com>
2024-03-26 10:43:21 +0100

    kalimi i te dhenave ne databaz

--- regjistrim.php
+++ regjistrim.php
@@ -1,10 +1,25 @@
 <?php
+$emri = $_POST['emri'];
+$mbiemri = $_POST['mbiemri'];
+$email = $_POST['email'];
+$pass = $_POST['password']; // Changed variable name to avoid conflict
 
 $servername = "localhost";
 $username = "root"; 
 $password = ""; 
 $database = "prov";
 
-$con=mysqli_connect($servername, $username, $password, $database) OR die ('Provo perseri');
+$con = mysqli_connect($servername, $username, $password, $database) or die('Provo perseri');
 
+$sql = "INSERT INTO users (emri, mbiemri, email, password) VALUES ('$emri', '$mbiemri', '$email', '$pass')"; // Fixed password variable usage
+
+// Execute SQL statement
+if ($con->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+
+// Close database connection
+$con->close();
 ?>
