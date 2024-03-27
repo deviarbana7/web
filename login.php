089da22233f4837cb0d6e2bcc7546fbc95da077d
Devi Arbana <deviarbana7@gmail.com>
2024-03-27 17:20:03 +0100

    gjys rregullimi per check dhe login

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php      
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$database = "prov";
+
+$con = mysqli_connect($servername, $username, $password, $database) or die('Provo perseri');
+
+    $email = $_POST['email'];  
+    $pass = $_POST['password'];  
+      
+        //to prevent from mysqli injection  
+        $email = stripcslashes($username);  
+        $pass = stripcslashes($pass);  
+        $username = mysqli_real_escape_string($con, $email);  
+        $pass = mysqli_real_escape_string($con, $pass);  
+      
+        $sql = "select *from users where email = '$email' and password = '$pass'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo " Login successful ";  
+        }  
+        else{  
+            echo " Login failed. Invalid username or password.";  
+        }     
+?>  
